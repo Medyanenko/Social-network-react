b16922818d335671d8cbdf41c200eec51cff5ee1
Medyanenko <t.kudryavska@gmail.com>
2022-06-09 10:56:08 +0200

    oop and add store

--- src/index.js
+++ src/index.js
@@ -3,24 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {addPost}from './redux/state';
-import {updateNewPostText} from './redux/state';
-import {addMessage}from './redux/state';
-import {updateNewMessageText} from './redux/state';
-import {subscribe}from './redux/state';
+import store from './redux/state';
 
 
-let renderEntireTree = (state) => {
-    ReactDOM.render(
+let rerenderEntireTree = (state) => {
+    ReactDOM.render( 
         <React.StrictMode> 
-          <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText}/>
+          <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} addMessage={store.addMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
       );
 reportWebVitals();
 }
-renderEntireTree(state);
-subscribe(renderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,7 +1,5 @@
-let renderEntireTree = () => {
-}
-
-let state = {
+let store = {
+    _state : {
     profilePage : {
         posts : [
             {id: 1, message: 'Hi', like: '15'},
@@ -30,39 +28,44 @@ let state = {
       { id: 3, name: "Masha", avatar: "http://игрушечный-рай.рф/img/cms/%D0%9C%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B/%D0%93%D0%B5%D1%80%D0%BE%D0%B8%20%D0%B2%20%D0%BC%D0%B0%D1%81%D0%BA%D0%B0%D1%85/%D0%90%D0%BC%D0%B0%D0%B9%D1%8F%20(%D0%90%D0%BB%D0%B5%D1%82%D1%82).jpg" },
       { id: 4, name: "Max", avatar: "http://игрушечный-рай.рф/img/cms/%D0%9C%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B/%D0%93%D0%B5%D1%80%D0%BE%D0%B8%20%D0%B2%20%D0%BC%D0%B0%D1%81%D0%BA%D0%B0%D1%85/%D0%93%D1%80%D0%B5%D0%B3%20(%D0%93%D0%B5%D0%BA%D0%BA%D0%BE).jpg" }
   ]
-}
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    like: 0
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText ='';
-  renderEntireTree(state);
+  },
+  getState(){
+    return this._state;
+  },
+  _callSubscriber(){
+  },
+  addPost(){
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      like: 0
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText ='';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newPost){
+    this._state.profilePage.newPostText = newPost;
+    this._callSubscriber(this._state);
+    
+  },
+  addMessage(){
+    let newMesage = {
+      id: 5,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messages.push(newMesage);
+    this._state.dialogsPage.newMessageText ='';
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newMessage){
+    this._state.dialogsPage.newMessageText = newMessage;
+    this._callSubscriber(this._state);  
+  },
+  subscribe(observer){
+    this._callSubscriber = observer;
+  }
 }
 
-export const updateNewPostText = (newPost) => {
-  state.profilePage.newPostText = newPost;
-  renderEntireTree(state);
-  
-}
-
-export const addMessage = () => {
-  let newMesage = {
-    id: 5,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messages.push(newMesage);
-  state.dialogsPage.newMessageText ='';
-  renderEntireTree(state);
-}
-
-export const updateNewMessageText = (newMessage) => {
-  state.dialogsPage.newMessageText = newMessage;
-  renderEntireTree(state);  
-}
-export const subscribe = (observer) =>{
-  renderEntireTree = observer;
-}
-export default state;
+export default store;
+window.store = store;
