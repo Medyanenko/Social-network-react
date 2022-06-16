e593351874eab24ad8db1110369cdbf22ff5800b
Medyanenko <t.kudryavska@gmail.com>
2022-06-16 16:15:11 +0200

    axios, request, api, server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import s from "./Users.module.css"
+import s from "./Users.module.css";
+import axios from 'axios';
+import userPhoto from "./../../assets/img/user.png"
 
 let Users = (props) => {
+ 
     if(props.users.length === 0){
-        props.setUsers(
-          [ {id: 1, photoUrl: 'https://vyazanoe-tvorchestvo.ru/wp-content/uploads/2020/07/JSOwXpwrSY0.jpg', followed: true, fullName: 'Tanya K', status: 'I am a front-end developer', location: {city: 'Vienna', country: 'Austria'}},
-             {id: 2, photoUrl: 'https://images.deal.by/86699515_86699515.jpg?PIMAGE_ID=86699515', followed: false, fullName: 'Vadym K', status: 'I am a boss', location: {city: 'Vienna', country: 'Austria'}},
-             {id: 3, photoUrl: 'http://игрушечный-рай.рф/img/cms/%D0%9C%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B/%D0%93%D0%B5%D1%80%D0%BE%D0%B8%20%D0%B2%20%D0%BC%D0%B0%D1%81%D0%BA%D0%B0%D1%85/%D0%90%D0%BC%D0%B0%D0%B9%D1%8F%20(%D0%90%D0%BB%D0%B5%D1%82%D1%82).jpg', followed: false, fullName: 'Masha K', status: 'I am a Oberlaa Princess', location: {city: 'Vienna', country: 'Austria'}},
-             {id: 4, photoUrl: 'http://игрушечный-рай.рф/img/cms/%D0%9C%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B/%D0%93%D0%B5%D1%80%D0%BE%D0%B8%20%D0%B2%20%D0%BC%D0%B0%D1%81%D0%BA%D0%B0%D1%85/%D0%93%D1%80%D0%B5%D0%B3%20(%D0%93%D0%B5%D0%BA%D0%BA%D0%BE).jpg', followed: false, fullName: 'Max K', status: 'I am a dragon', location: {city: 'Vienna', country: 'Austria'}}
-           ]
-        )
+      
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+            props.setUsers(response.data.items);
+        });    
     }
     return <div>
         {
             props.users.map( u => <div key={u.id} >
                 <span>
                     <div>
-                        <img src = {u.photoUrl} className={s.userPhoto}/>
+                        <img src = {u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
                     </div>
                     <div>
                         { u.followed 
@@ -27,7 +28,7 @@ let Users = (props) => {
                 <span>
                     <span>
                         <div>
-                            {u.fullmame}
+                            {u.name}
                         </div>
                         <div>
                             {u.status}
@@ -35,10 +36,10 @@ let Users = (props) => {
                     </span>
                     <span>
                         <div>
-                            {u.location.country}
+                            {'u.location.country'}
                         </div>
                         <div>
-                            {u.location.city}
+                            {'u.location.city'}
                         </div>
                     </span>
                 </span>
