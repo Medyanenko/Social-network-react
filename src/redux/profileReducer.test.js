8cb5e738aff00d3d414d50ea645030c7de54afab
Medyanenko <t.kudryavska@gmail.com>
2022-08-10 22:44:46 +0200

    add simple test

---
+++ src/redux/profileReducer.test.js
@@ -0,0 +1,45 @@
+import profileReducer, {addPostActionCreator} from './profileReducer'
+
+
+let state = {
+    posts: [{
+            id: 1,
+            message: 'Hi',
+            like: '15'
+        },
+        {
+            id: 2,
+            message: 'Hola',
+            like: '20'
+        }
+    ],
+}
+
+
+test('length of posts should be incremented', () => {
+   let action = addPostActionCreator('aaa')
+   let newState = profileReducer(state, action)
+   expect(newState.posts.length).toBe(3)
+
+})
+
+
+  test('message of posts should be correct', () => {
+      let action = addPostActionCreator('aaa')
+      let newState = profileReducer(state, action)
+      expect(newState.posts[2].message).toBe('aaa')
+ })
+
+
+// test('after deleting length of message should be decrement', () => {
+//    let action = deletePost(1)
+//    let newState = profileReducer(state, action)
+//    expect(newState.posts.length).toBe(2)
+// })
+
+// test(`after deleting length shouldn't be decrement if id incorrect`,
+//    () => {
+//       let action = actions.deletePost(555)
+//       let newState = profileReducer(state, action)
+//       expect(newState.posts.length).toBe(3)
+//    })
