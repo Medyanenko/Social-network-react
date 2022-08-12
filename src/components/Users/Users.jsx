9481c6aa2b68de48b963966fa9289479f36e9b87
Medyanenko <t.kudryavska@gmail.com>
2022-08-12 20:46:15 +0200

    add paginator

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,7 +6,7 @@ import User from "./User"
 let Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props}) => {
   return (
     <div>
-      <Paginator currentPage={currentPage} totalUsersCount={totalUsersCount} pageSize={pageSize} onPageChanged={onPageChanged}/>
+      <Paginator currentPage={currentPage} totalItemsCount={totalUsersCount} pageSize={pageSize} onPageChanged={onPageChanged}/>
       <div>
         {users.map((u) => (<User user={u} key={u.id} followingInProgress={props.followingInProgress} unfollow={props.unfollow} follow={props.follow}/>
         ))}
